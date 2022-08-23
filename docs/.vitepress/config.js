ee7436b162538d719ca9c537e9312ecca3c3d44b
王鑫宇 <937219635@qq.com>
2022-08-23 15:05:26 +0800

    modify:nav-bar

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,7 @@
  * @Author: 王鑫宇 937219635@qq.com
  * @Date: 2022-08-21 11:21:28
  * @LastEditors: 王鑫宇 937219635@qq.com
- * @LastEditTime: 2022-08-23 14:59:52
+ * @LastEditTime: 2022-08-23 15:05:06
  * @FilePath: \blog-mine\docs\.vitepress\config.js
  * @Description: 
  * 
@@ -22,10 +22,10 @@ export default defineConfig({
     themeConfig: {
         logo: "/logo.svg",
         nav: [
-            { text: "首页11", link: "/" },
+            { text: "首页", link: "/" },
             { text: "我的笔记", link: "/daily/" },
-            { text: "前端总结", link: "/javascript/" },
-            { text: "生活感悟", link: "/life/" },
+            { text: "前端面试", link: "/javascript/" },
+            { text: "react学习", link: "/life/" },
             { text: "我的掘金", link: "https://juejin.cn/user/3483706528631166" },
             { text: "关于我", link: "/me/" },
         ],
